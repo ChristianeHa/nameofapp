990dd1fa0d350504ffefb01e53bff2c099efcbe3
Christiane Hasslberger <christianehasslberger@web.de>
2017-02-15 10:06:14 +0100

    5.2 Search Form added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,20 @@
 class Product < ApplicationRecord
 	has_many :orders
+
+	def next
+		Product.where("id > ?", id).first
+	end
+
+	def prev
+		Product.where("id < ?", id).last
+	end
+
+	def self.search(search_term)
+		if Rails.env.production?
+        	@products = Product.where("name ilike ?", "%#{search_term}")
+      	else
+        	@products = Product.where("name LIKE ?", "%#{search_term}")
+      	end
+	end
+
 end
