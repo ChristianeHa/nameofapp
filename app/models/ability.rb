5c0ab37e67c9bafe18323db49d14b953a00e818f
Christiane Hasslberger <christianehasslberger@web.de>
2017-03-09 17:57:37 +0100

    5.10 added admin role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,10 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.admin?
+    	can :delete, User, id: user.id
+    else
+    	can :read, User, id: user.id
+    end
   end
 end
