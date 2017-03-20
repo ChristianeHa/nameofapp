df01f37c8388d4202556af8c369dd45d9287ce99
Christiane Hasslberger <christianehasslberger@web.de>
2017-03-20 12:54:47 +0100

    6.6 added Stripe Controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -10,17 +10,45 @@ class PaymentsController < ApplicationController
       			:amount => @newprice, # amount in cents, again
       			:currency => "EUR",
       			:source => token,
-      			:description => params[:stripeEmail],
+      			:description => params[:stripeEmail]
     		)
 
-        # if charge.paid
-        #   Order.create(:product_id => @product.id, :user_id => @user.id, :total => "1")
-        # end
+        if charge.paid
+          Order.create(:product_id => @product.id, :user_id => @user.id, :total => @newprice)
+        end
 
   		rescue Stripe::CardError => e
     		body = e.json_body
     		err = body[:error]
     		flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+
+      rescue Stripe::RateLimitError => e
+        body = e.json_body
+        err = body[:error]
+        flash[:error] = "If you are not a robot, you were to quick for our system: #{err[:message]}"
+
+      rescue Stripe::InvalidRequestError => e
+        body = e.json_body
+        err = body[:error]
+        flash[:error] = "Uuups, there was something wrong with your input: #{err[:message]}"
+
+      rescue Stripe::AuthenticationError => e
+        body = e.json_body
+        err = body[:error]
+        flash[:error] = "There is something wrong with our form - sorry!: #{err[:message]}"
+
+      rescue Stripe::AuthenticationError => e
+        body = e.json_body
+        err = body[:error]
+        flash[:error] = "Are you online? We can't reach Stripe at the moment: #{err[:message]}"
+
+      rescue Stripe::AuthenticationError => e
+        body = e.json_body
+        err = body[:error]
+        flash[:error] = "Something went wrong (We don't know what it is either, don't worry): #{err[:message]}"
+
+      rescue => e
+
     	end
 	end
 end
