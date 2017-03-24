eb36e27368832b267e4639f188ad49aefaeb5191
Christiane Hasslberger <christianehasslberger@web.de>
2017-03-24 08:58:07 +0100

    6.10 AJAX

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,16 +1,22 @@
-$(document).on('turbolinks:load', function(){
 
-	//Raty
-    $('.rating').raty({ path: '/assets', scoreName: 'comment[rating]'});
-
+var refreshRating = function() {
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
+        readOnly: true,
+        score: function() {
+            return $(this).attr('data-score');
+        }
     });
+};
+
+
+$(document).on('turbolinks:load ajaxSuccess', function(){
+
+	   //Raty
+    refreshRating();
 
     //Zoom Plugin for Product Pictures
 
     $('.img-zoom').elevateZoom();
+
 });
