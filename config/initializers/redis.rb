9016de73173c09af1c2bb7e1e57ad4d5ca4a4af4
Christiane Hasslberger <christianehasslberger@web.de>
2017-03-27 20:33:14 +0200

    6.12 added Redis on Heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,11 @@
 
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+if Rails.env.production?
+
+	$redis = Redis.new(url: ENV["redis://h:p8a2e8f271838ea487abffe008799a1671d1233c85acb8b1e9e2592ce41c35e4a@ec2-34-206-56-226.compute-1.amazonaws.com:23999"])
+
+else
+
+
+	$redis = Redis.new(:host => 'localhost', :port => 6379)
+
+end
